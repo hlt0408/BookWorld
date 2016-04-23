17392f8a74bc74f0f939f215ab589e25a1c4c9a5
hlt0408 <huanglangtao08@gmail.com>
2016-04-23 10:30:29 -0400

    front end nav

--- src/java/controller/ControllerServlet.java
+++ src/java/controller/ControllerServlet.java
@@ -5,7 +5,6 @@ package controller;
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-
 import java.io.IOException;
 import java.io.PrintWriter;
 import javax.servlet.ServletException;
@@ -19,11 +18,12 @@ import javax.servlet.http.HttpServletResponse;
  * @author hlt04
  */
 @WebServlet(loadOnStartup = 1, urlPatterns = {"/aboutus", "/contactus", "/inventory", "/item", "/addToCart", "/viewCart",
-                           "/updateCart", "/checkout", "/purchase", "/change-password", "/purchase-history", "/user-board"})
+    "/updateCart", "/checkout", "/purchase", "/change-password", "/purchase-history", "/user-board"})
 public class ControllerServlet extends HttpServlet {
 
     /**
      * Handles the HTTP <code>GET</code> method.
+     *
      * @param request servlet request
      * @param response servlet response
      * @throws ServletException if a servlet-specific error occurs
@@ -31,7 +31,7 @@ public class ControllerServlet extends HttpServlet {
      */
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response)
-    throws ServletException, IOException {
+            throws ServletException, IOException {
 
         String userPath = request.getServletPath();
 
@@ -39,28 +39,32 @@ public class ControllerServlet extends HttpServlet {
         if (userPath.equals("/inventory")) {
             // TODO: Implement inventory request
             userPath = "/inventory";
+            request.setAttribute("title", "inventory");
+            request.setAttribute("inventoryactive", true);
+
         } else if (userPath.equals("/item")) {
             // TODO: Implement item page request
-
+            request.setAttribute("title", "item");
             userPath = "/item";
-        // if aboutus page is requested
+            // if aboutus page is requested
         } else if (userPath.equals("/aboutus")) {
             // TODO: Implement aboutus page request
-
+            request.setAttribute("title", "aboutus");
+            request.setAttribute("aboutactive", true);
             userPath = "/aboutus";
         } else if (userPath.equals("/contactus")) {
             // TODO: Implement contactus page request
+            request.setAttribute("title", "contactus");
+            request.setAttribute("contactusactive", true);
 
             userPath = "/contactus";
 
-        // if checkout page is requested
+            // if checkout page is requested
         } else if (userPath.equals("/checkout")) {
             // TODO: Implement checkout page request
-
-        // if user switches language
-        } else if (userPath.equals("/chooseLanguage")) {
-            // TODO: Implement language request
-
+            request.setAttribute("title", "checkout");
+            userPath = "/checkout";
+            // if user switches language
         }
 
         // use RequestDispatcher to forward request internally
@@ -75,6 +79,7 @@ public class ControllerServlet extends HttpServlet {
 
     /**
      * Handles the HTTP <code>POST</code> method.
+     *
      * @param request servlet request
      * @param response servlet response
      * @throws ServletException if a servlet-specific error occurs
@@ -82,28 +87,41 @@ public class ControllerServlet extends HttpServlet {
      */
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response)
-    throws ServletException, IOException {
+            throws ServletException, IOException {
 
         String userPath = request.getServletPath();
 
         // if addToCart action is called
         if (userPath.equals("/addToCart")) {
             // TODO: Implement add product to cart action
-
-        // if updateCart action is called
+            request.setAttribute("title", "addToCart");
+            userPath = "/addToCart";
+            
+            // if updateCart action is called
         } else if (userPath.equals("/updateCart")) {
             // TODO: Implement update cart action
-
-        // if purchase action is called
+            request.setAttribute("title", "updateCart");
+            userPath = "/updateCart";
+            // if purchase action is called
         } else if (userPath.equals("/purchase")) {
             // TODO: Implement purchase action
-
-            userPath = "/confirmation";
+            request.setAttribute("title", "purchase");
+            userPath = "/purchase";
+            
         } else if (userPath.equals("/change-password")) {
             userPath = "/change-password";
+            request.setAttribute("changepasswordactive", true);
+            request.setAttribute("title", "change-password");
+            
         } else if (userPath.equals("/purchase-history")) {
             userPath = "/purchase-history";
+            request.setAttribute("purchasehistoryctive", true);
+            request.setAttribute("title", "Purchase History");
+            
         } else if (userPath.equals("/user-board")) {
+            userPath = "/user-board";
+            request.setAttribute("title", "user-board");
+            request.setAttribute("userboardactive", true);
             userPath = "/user-board";
         }
 
